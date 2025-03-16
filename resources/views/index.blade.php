b5b416380ec5e6a5c421e4d819838494cafc7a75
samiulbehindyou <shamiul982@gmail.com>
2025-03-16 10:28:43 +0600

    jetstream insertia

---
+++ resources/views/index.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel 12</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row mt-2">
+            <div class="col-md-9 m-auto">
+                <div class="card">
+                    <div class="card-header bg-warning">
+                        <h3 class="text-center text-white">Laravel 12</h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="my-1">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur amet eaque distinctio laborum iusto. Maxime veritatis necessitatibus minima expedita dolorum quos tempora numquam possimus aperiam asperiores magnam quae est voluptates cupiditate consequatur id animi atque, dolor sint sit excepturi error. Voluptatibus assumenda neque harum repellendus impedit praesentium, sed quaerat reiciendis cum? Delectus eum provident cumque labore earum doloremque iste? Tempora quasi ea cumque consectetur corporis dolorum enim ut unde commodi illum odit eius delectus similique error, quam quibusdam quos inventore, vitae aperiam totam est expedita pariatur voluptatem ipsa. Voluptatibus soluta quaerat est libero voluptate, vel in magnam sunt sapiente mollitia.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+</html>
